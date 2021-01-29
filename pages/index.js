44a2c941fce4a18f8f6451f13739e6f4d9017a27
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-29 14:29:11 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -48,8 +48,23 @@ export default function Home() {
         </Widget>
         <Widget>
           <Widget.Content>
-            <h1>Grand Theft Auto</h1>
-            <p>lorem ipsum dolor sit amet, consectetur adip</p>
+            <ul>
+              {db.external.map( link => {
+                const [projectName, githubUser] = link
+                                .replace(/\//g, '')
+                                .replace('https:' || 'http:', '')
+                                .replace('.vercel.app', '')
+                                .split('.')
+                return( 
+                  <li key={link}>
+                    <Widget.Topic href={link}>
+                      {` ${githubUser} - ${projectName} `}
+                    </Widget.Topic>
+                  </li>
+                  
+                )
+              })}
+            </ul>
           </Widget.Content>
         </Widget>
         <Footer />
