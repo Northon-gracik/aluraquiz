dd7150cdfd9300066c747475d613ef504b1a8f3f
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-02-02 15:57:09 -0300

    Update [id].js

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -10,27 +10,27 @@ function QuizDaGaleraPage (props) {
     );
 }
 
-// QuizDaGaleraPage.getInitialProps = async (context) => {
-//     const dbExterno = await fetch/*(`https://${projectName}.${githubUser}.vercel.app/api/db`)*/("https://aluraquiz-css.omariosouto.vercel.app/api/db")
-//     .then((respostaDoServer) => {
-//         if (respostaDoServer.ok) {
-//             return respostaDoServer.json();
-//         }
-//         throw new Error('Falha em pegar os dados');
-//     })
-//     .then((respostaConvertidaEmObjeto) => respostaConvertidaEmObjeto)
-//     .catch((err) => {
-//         console.error(err);
-//     });
+QuizDaGaleraPage.getInitialProps = async (context) => {
+    const dbExterno = await fetch/*(`https://${projectName}.${githubUser}.vercel.app/api/db`)*/("https://aluraquiz-css.omariosouto.vercel.app/api/db")
+    .then((respostaDoServer) => {
+        if (respostaDoServer.ok) {
+            return respostaDoServer.json();
+        }
+        throw new Error('Falha em pegar os dados');
+    })
+    .then((respostaConvertidaEmObjeto) => respostaConvertidaEmObjeto)
+    .catch((err) => {
+        console.error(err);
+    });
 
-//      console.log('dbExterno', dbExterno);
-//     // console.log('Infos que o Next da para nós', context.query.id);
-//     return {
-//       props: {
-//         dbExterno,
-//       },
-//     };
-// }
+     console.log('dbExterno', dbExterno);
+    // console.log('Infos que o Next da para nós', context.query.id);
+    return {
+      props: {
+        dbExterno,
+      },
+    };
+}
 
 
 export default QuizDaGaleraPage;
